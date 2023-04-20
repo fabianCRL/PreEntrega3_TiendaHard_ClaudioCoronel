a450d0c7dc40feb1e79a16299d318a6a3c7142c8
fabianCRL <fabian_0328@hotmail.com>
2023-04-19 23:25:57 -0300

    PreEntrega 3

--- main.js
+++ main.js
@@ -15,7 +15,40 @@ function cambiarTema() {
     else {
         btnCambiarTema.innerText = ("Tema Oscuro");
     }
-    
+
 
 }
 
+// --------- Ingresar Usuario-----------
+
+const formulario = document.querySelector('form');
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombre= document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const bienvenido = document.querySelector('h2');
+    
+const agregarUsuario = {
+        nombre,
+        email,
+    };
+
+    let usuarios = [];
+
+    if (localStorage.getItem('usuarios')) {
+        usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    }
+
+    usuarios.push(agregarUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    bienvenido.textContent = "Bienvenid@ " + nombre; 
+
+    console.log('Usuario agregado correctamente');
+
+    formulario.reset();
+});
